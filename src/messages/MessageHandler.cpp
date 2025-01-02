5d79fe0d29556f0d7a05114cf19d025e07ea7823
Tiểu Phương <tieuphuong@Tieus-Mac.local>
2025-01-02 11:38:50 +0700

    Edit MessageHandler

--- src/messages/MessageHandler.cpp
+++ src/messages/MessageHandler.cpp
@@ -6,6 +6,8 @@
 #include "../models/header/RoomParticipant.h"
 #include "../models/header/PlayerGameSession.h"
 #include "../models/header/GameSession.h"
+#include <unordered_map>
+#include <functional>
 
 json MessageHandler::parseMessage(const std::string& message) {
     return json::parse(message);
@@ -15,38 +17,30 @@ json MessageHandler::handleMessage(const json& parsed_message, sqlite3* db) {
     std::string type = parsed_message["type"];
     json response_data;
 
-    if (type == "SIGN_UP") {
-        response_data = handleSignUp(parsed_message, db);
-    } else if (type == "SIGN_IN") {
-        response_data = handleSignIn(parsed_message, db);
-    } else if (type == "VIEW_ROOMS") {
-        response_data = handleViewRooms(parsed_message, db);
-    } else if (type == "CREATE_ROOM") {
-        response_data = handleCreateRoom(parsed_message, db);
-    } else if (type == "JOIN_ROOM") {
-        response_data = handleJoinRoom(parsed_message, db);
-    } else if (type == "LEAVE_ROOM") {
-        response_data = handleLeaveRoom(parsed_message, db);
-    } else if (type == "READY") {
-        response_data = handleReady(parsed_message, db);
-    } else if (type == "START_GAME") {
-        response_data = handleStartGame(parsed_message, db);
-    } else if (type == "CUBE_UPDATE") {
-        response_data = handleCube(parsed_message, db);
-    } else if (type == "END_GAME") {
-        response_data = handleEndGame(parsed_message, db);
-    } else if (type == "VIEW_USERS") {
-        response_data = handleViewUsers(parsed_message, db);
-    } else if (type == "BAN_PLAYER") {
-        response_data = handleBanPlayer(parsed_message, db);
-    } else if (type == "SPECTATE") {
-        // response_data = handleSpectate(parsed_message, db);
-    } else if (type == "READY") {
-        response_data = handleReady(parsed_message, db);
+    static const std::unordered_map<std::string, HandlerFunction> handlers = {
+        {"SIGN_UP", [this](const json& msg, sqlite3* db) { return handleSignUp(msg["data"], db); }},
+        {"SIGN_IN", [this](const json& msg, sqlite3* db) { return handleSignIn(msg["data"], db); }},
+        {"VIEW_ROOMS", [this](const json& msg, sqlite3* db) { return handleViewRooms(msg["data"], db); }},
+        {"CREATE_ROOM", [this](const json& msg, sqlite3* db) { return handleCreateRoom(msg["data"], db); }},
+        {"JOIN_ROOM", [this](const json& msg, sqlite3* db) { return handleJoinRoom(msg["data"], db); }},
+        {"LEAVE_ROOM", [this](const json& msg, sqlite3* db) { return handleLeaveRoom(msg["data"], db); }},
+        {"READY", [this](const json& msg, sqlite3* db) { return handleReady(msg["data"], db); }},
+        {"START_GAME", [this](const json& msg, sqlite3* db) { return handleStartGame(msg["data"], db); }},
+        {"CUBE_UPDATE", [this](const json& msg, sqlite3* db) { return handleCube(msg["data"], db); }},
+        {"END_GAME", [this](const json& msg, sqlite3* db) { return handleEndGame(msg["data"], db); }},
+        {"VIEW_USERS", [this](const json& msg, sqlite3* db) { return handleViewUsers(msg["data"], db); }},
+        {"BAN_PLAYER", [this](const json& msg, sqlite3* db) { return handleBanPlayer(msg["data"], db); }},
+        {"SPECTATE", [this](const json& msg, sqlite3* db) { return handleSpectate(msg["data"], db); }},
+        {"READY", [this](const json& msg, sqlite3* db) { return handleReady(msg["data"], db); }}
+    };
+
+    auto it = handlers.find(type);
+    if (it != handlers.end()) {
+        response_data = it->second(parsed_message, db);
+        return craftResponse("success", response_data);
     } else {
         return craftResponse("error", {{"message", "Unknown message type"}});
     }
-    return craftResponse("success", response_data);
 }
 
 json MessageHandler::craftResponse(const std::string& status, const json& data) {
@@ -120,4 +114,11 @@ json MessageHandler::handleEndGame(const json& parsed_message, sqlite3* db) {
     GameSession gameSession(db);
     int player_id = parsed_message["player_id"];
     return gameSession.endGameSession(player_id);
+}
+
+json MessageHandler::handleSpectate(const json& parsed_message, sqlite3* db) {
+    Admin admin(db);
+    int game_session_id = parsed_message["game_session_id"];
+    int room_id = parsed_message["room_id"];
+    return admin.spectate(game_session_id, room_id);
 }
